0123348f3fe6742b81f7a6b9b3116b5589ec25fa
Jan Lána <jan.lana@gyarab.cz>
2021-05-07 07:05:59 +0200

    verze zaslaná emailem

---
+++ src/pong1/Game.java
@@ -0,0 +1,13 @@
+package pong1;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Game {
+
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame();
+    }
+}
